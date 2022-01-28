0f6f908acd4c12cf149409e0dd6f01dbad9caa75
Surya Prakash Ganesan (MS/ECB32-PS) <SuryaPrakash.Ganesan@in.bosch.com>
2022-01-28 14:37:17 +0530

    try 2

--- combined.ps1
+++ combined.ps1
@@ -36,9 +36,16 @@ function Get-ClusterCapacityCheck {
     
     $VMHost = $Cluster | Get-VMHost | Select-Object -Last 1
     $ClusterFreeDiskspaceGB = ($VMHost | Get-Datastore | Where-Object {$_.Extensiondata.Summary.MultipleHostAccess -eq $True} | Measure-Object -Property FreeSpaceGB -Sum).Sum
-    $ClusterCPUUsageMHz = ($VMHost | Get-Datastore | Where-Object {$_.Extensiondata.Summary.MultipleHostAccess -eq $True} | Measure-Object -Property CpuUsageMhz -Sum).Sum
-    $ClusterCPUTotalMHz = ($VMHost | Get-Datastore | Where-Object {$_.Extensiondata.Summary.MultipleHostAccess -eq $True} | Measure-Object -Property CpuTotalMhz -Sum).Sum
+    $ClusterCPUUsageMHz = ($VMHost | Measure-Object -Property CpuUsageMhz -Sum).Sum
+    $ClusterCPUTotalMHz = ($VMHost | Measure-Object -Property CpuTotalMhz -Sum).Sum
+    $ClusterNumCPU = ($VMHost | Measure-Object -Property NumCpu -Sum).Sum
+    $ClusterTotalMemory = ($VMHost | Measure-Object -Property MemoryTotalGB -Sum).Sum
+    $ClusterUsedMemory = ($VMHost | Measure-Object -Property MemoryUsageGB -Sum).Sum
     
+    $ClusterFreeCPUGHz = Round(($ClusterCPUTotalMHz - $ClusterCPUUsageMHz)/1000,2)
+    $ClusterFreeCPUGHz_PerCore = Round(($ClusterFreeCPUGHz/ ($ClusterCPUTotalMHz/(1000*$ClusterNumCPU))),2)
+    $ClusterMemory = $ClusterTotalMemory - $ClusterUsedMemory
+
     New-Object -TypeName PSObject -Property @{
     Cluster = $Cluster.Name
     #ClusterCPUCores = $ClusterCPUCores
@@ -50,9 +57,24 @@ function Get-ClusterCapacityCheck {
     ClusterFreeDiskspaceGB = $ClusterFreeDiskspaceGB
     ClusterCPUUsageMHz = $ClusterCPUUsageMHz
     ClusterCPUTotalMHz = $ClusterCPUTotalMHz
+    ClusterFreeCPUGHz = $ClusterFreeCPUGHz
+    ClusterFreeCPUGHz_PerCore = $ClusterFreeCPUGHz_PerCore
+    ClusterMemory = $ClusterMemory
+
     }
     }
     }
     #Get-Cluster | Get-ClusterCapacityCheck | Select-Object Cluster,ClusterCPUCores,ClusterAllocatedvCPUs,ClustervCPUpCPURatio,ClusterEffectiveMemoryGB,ClusterAllocatedMemoryGB,ClusterActiveMemoryPercentage,ClusterFreeDiskspaceGB
-    Get-Cluster | Get-ClusterCapacityCheck | Select-Object Cluster,ClusterFreeDiskspaceGB,ClusterCPUUsageMHz,ClusterCPUTotalMHz
+    Get-Cluster | Get-ClusterCapacityCheck | Select-Object Cluster,ClusterFreeDiskspaceGB,ClusterCPUUsageMHz,ClusterCPUTotalMHz,ClusterFreeCPUGHz,ClusterFreeCPUGHz_PerCore,ClusterMemory
+    
+
+
+    @{N='Used CPU(GHz)';E={[math]::Round($_.CpuUsageMhz/1000,2)}},
+    @{N='Free CPU(GHz)';E={[math]::Round(($_.CpuTotalMhz - $_.CpuUsageMhz)/1000,2)}},
+    @{N='Free CPU(/Core)';E={[math]::Round(((($_.CpuTotalMhz - $_.CpuUsageMhz)/1000)/($_.CpuTotalMhz/(1000*$_.NumCpu))),0)}},
+    @{N='Total Memory(GB)';E={[math]::Round($_.MemoryTotalGB,2)}},
+    #@{N='Memory Used GB';E={[math]::Round($_.MemoryUsageGB,2)}},
+    @{N='Free Memory(GB)';E={[math]::Round(($_.MemoryTotalGB - $_.MemoryUsageGB),0)}} |
+    Export-Csv -Path C:\Materials\Downloads\capacityfinalreport-master\report.csv -NoTypeInformation -UseCulture
+    
     
